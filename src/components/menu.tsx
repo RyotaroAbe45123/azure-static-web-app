263f7a1c8636b38e8a166109e7a41056f55be902
kasama <kackoh40621@outlook.jp>
2023-06-02 11:06:41 +0900

    add logo

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -89,15 +89,8 @@ export const Menu = ({
 
   return (
     <>
-      <div className="absolute z-10 m-24">
+      <div className="absolute right-0 z-10 m-24">
         <div className="grid grid-flow-col gap-[8px]">
-          <IconButton
-            iconName="24/Menu"
-            className="bg-primary bg-opacity-0"
-            label="設定"
-            isProcessing={false}
-            onClick={() => setShowSettings(true)}
-          ></IconButton>
           {showChatLog ? (
             <IconButton
               iconName="24/CommentOutline"
@@ -116,6 +109,13 @@ export const Menu = ({
               onClick={() => setShowChatLog(true)}
             />
           )}
+          <IconButton
+            iconName="24/Menu"
+            className="bg-primary bg-opacity-0"
+            label="設定"
+            isProcessing={false}
+            onClick={() => setShowSettings(true)}
+          ></IconButton>
         </div>
       </div>
       {showChatLog && <ChatLog messages={chatLog} />}
